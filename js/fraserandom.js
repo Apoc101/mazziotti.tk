7ffb5a9ba5f2df269a2170c3901f9e68e0c11bd5
MatPrayer <61880817+MatPrayer@users.noreply.github.com>
2020-11-30 18:13:44 +0100

    Update fraserandom.js

--- js/fraserandom.js
+++ js/fraserandom.js
@@ -1,1 +1,220 @@
-var frase = [  "Raga shrekko",  "L'elicottero sta discendendo!",  "C'è pioggia tanta.",  "Cavo ti rendi conto che è tutto pavimento?",  "Sei stato invitato al seguente invito.",  "Ho i denti concavi.",  "Fartboot",  "Adesso Sesso",  "Sesso Adesso",  "Ma sissi dai!",    "C'è odore di puzza qua dentro",    "Hanno chiuso le discoteche e le scuole no pero' sono la stessa cosa",    "È Despair",      "Gli scambi commerciali",      "Conte ha detto che il 100% delle lezioni online saranno a scuola",      "Dai Bob, droppa le piastrelle",      "Parla e stai zitto",      "È chiaro che sono gay se lo spammi 400 volte",      "Volete vedere il mio tavolo potabile?",      "Hey guys, temperatures are pretty low here VAFFANCULO UAAAAAAAAAAAAAAAAAAAAA",    "Simo tu domani lo porti il grembiule?",    "Il mitico salto del papa",    "Fammi vedere il retto di fuoco",    "Raga il mirino elettrico",    "Forse stava indossando un costume da gay",    "Voglio piangere mentre esplodo",    "Perché, come sapete, mia madre è la basilicata"];var cit = [  "Cavo, 2020",  "MatPrayer, 2020",  "MatPrayer, 2020",  "Simone, 2020",  "Lore, 2020",  "Gab, 2020",  "Tommasyno, 2020",  "Simone, 2020",  "Simone, 2020",  "Simone, 2020",    "Gab, 2020",    "Cavo, 2020",    "Tutti, 2020",      "Gab, 2020",            "Gab, 2020",            "Gab, 2020",            "Simone, 2020",            "Cavo, 2020",            "Gab, 2020",            "Cavo, 2020",        "Cavo, 2020",        "Pier, 2020",        "Pier, 2020",        "Simo, 2020",        "Gab, 2020",        "Gab, 2020",        "Cavo, 2020"    ]//Non va fatto così, ma funzionavar randomNumber = Math.floor(Math.random()*frase.length);var fraserandom = frase[randomNumber];var citrandom = cit[randomNumber];document.getElementById("fraserandom").textContent=fraserandom;document.getElementById("citrandom").textContent=citrandom;
+var frase = [
+
+
+
+  "Raga shrekko",
+
+
+
+  "L'elicottero sta discendendo!",
+
+
+
+  "C'è pioggia tanta.",
+
+
+
+  "Cavo ti rendi conto che è tutto pavimento?",
+
+
+
+  "Sei stato invitato al seguente invito.",
+
+
+
+  "Ho i denti concavi.",
+
+
+
+  "Fartboot",
+
+
+
+  "Adesso Sesso",
+
+
+
+  "Sesso Adesso",
+
+
+
+  "Ma sissi dai!",
+
+  
+
+  "C'è odore di puzza qua dentro",
+
+  
+
+  "Hanno chiuso le discoteche e le scuole no pero' sono la stessa cosa",
+
+  
+
+  "È Despair",
+  
+  
+  "Gli scambi commerciali",
+  
+  
+  "Conte ha detto che il 100% delle lezioni online saranno a scuola",
+  
+  
+  "Dai Bob, droppa le piastrelle",
+  
+  
+  "Parla e stai zitto",
+  
+  
+  "È chiaro che sono gay se lo spammi 400 volte",
+  
+  
+  "Volete vedere il mio tavolo potabile?",
+  
+  
+  "Hey guys, temperatures are pretty low here VAFFANCULO UAAAAAAAAAAAAAAAAAAAAA",
+  
+  "Simo tu domani lo porti il grembiule?",
+  
+  "Il mitico salto del papa",
+  
+  "Fammi vedere il retto di fuoco",
+  
+  "Raga il mirino elettrico",
+  
+  "Forse stava indossando un costume da gay",
+  
+  "Voglio piangere mentre esplodo",
+  
+  "Perché, come sapete, mia madre è la basilicata",
+  
+  "Ho una voglia esplodente",
+
+
+
+];
+
+
+
+var cit = [
+
+
+
+  "Cavo, 2020",
+
+
+
+  "MatPrayer, 2020",
+
+
+
+  "MatPrayer, 2020",
+
+
+
+  "Simone, 2020",
+
+
+
+  "Lore, 2020",
+
+
+
+  "Gab, 2020",
+
+
+
+  "Tommasyno, 2020",
+
+
+
+  "Simone, 2020",
+
+
+
+  "Simone, 2020",
+
+
+
+  "Simone, 2020",
+
+  
+
+  "Gab, 2020",
+
+  
+
+  "Cavo, 2020",
+
+  
+
+  "Tutti, 2020",
+  
+
+    "Gab, 2020",
+    
+    
+    "Gab, 2020",
+    
+    
+    "Gab, 2020",
+    
+    
+    "Simone, 2020",
+    
+    
+    "Cavo, 2020",
+    
+    
+    "Gab, 2020",
+    
+    
+    "Cavo, 2020",
+    
+    "Cavo, 2020",
+    
+    "Pier, 2020",
+    
+    "Pier, 2020",
+    
+    "Simo, 2020",
+    
+    "Gab, 2020",
+    
+    "Gab, 2020",
+    
+    "Cavo, 2020",
+  
+    "Gab, 2020",
+    
+
+]
+
+
+
+//Non va fatto così, ma funziona
+
+
+
+var randomNumber = Math.floor(Math.random()*frase.length);
+
+
+
+var fraserandom = frase[randomNumber];
+
+
+
+var citrandom = cit[randomNumber];
+
+
+
+
+
+
+
+
+
+document.getElementById("fraserandom").textContent=fraserandom;
+
+
+
+document.getElementById("citrandom").textContent=citrandom;
