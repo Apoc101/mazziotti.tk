e7fbd13c4f47c4e0fbae4ff200e7539445ee2a42
MatPrayer <61880817+MatPrayer@users.noreply.github.com>
2021-04-19 21:20:47 +0200

    simo fa molto schifo, quindi sono costretto a fare sta roba
    simo nabbo

--- js/fraserandom.js
+++ js/fraserandom.js
@@ -1,305 +1,717 @@
 var frase = [
 
 
+    "Raga shrekko",
+  
+  
+  
+    "L'elicottero sta discendendo!",
+  
+  
+  
+    "C'è pioggia tanta.",
+  
+  
+  
+    "Cavo, qui che è tutto pavimento",
+  
+  
+  
+    "Sei stato invitato al seguente invito.",
+  
+  
+  
+    "Ho i denti concavi.",
+  
+  
+  
+    "Fartboot",
+  
+  
+  
+    "Adesso Sesso",
+  
+  
+  
+    "Sesso Adesso",
+  
+  
+  
+    "Ma sissi dai!",
+  
+    
+  
+    "C'è odore di puzza qua dentro",
+  
+    
+  
+    "Hanno chiuso le discoteche e le scuole no pero' sono la stessa cosa",
+  
+    
+  
+    "È Despair",
+    
+    
+    "Gli scambi commerciali",
+    
+    
+    "Conte ha detto che il 100% delle lezioni online saranno a scuola",
+    
+    
+    "Dai Bob, droppa le piastrelle",
+    
+    
+    "Parla e stai zitto",
+    
+    
+    "È chiaro che sono gay se lo spammi 400 volte",
+    
+    
+    "Volete vedere il mio tavolo potabile?",
+    
+    
+    "Hey guys, temperatures are pretty low here VAFFANCULO UAAAAAAAAAAAAAAAAAAAAA",
+    
+    "Simo tu domani lo porti il grembiule?",
+    
+    "Il mitico salto del papa",
+    
+    "Fammi vedere il retto di fuoco",
+    
+    "Raga il mirino elettrico",
+    
+    "Forse stava indossando un costume da gay",
+    
+    "Voglio piangere mentre esplodo",
+    
+    "Perché, come sapete, mia madre è la basilicata",
+    
+    "Ho una voglia esplodente",
+    
+    "Quando esce da questo if del sesso",
+    
+    "Simo ha tipo tre porte di finestre",
+    
+    "Ma le porte finestre sono quelle che portano le finestre",
+    
+    "Jacopo, casa tua è scale",
+    
+    "Gab, quanti alberi hai crocifisso?",
+    
+    "Sto uccidendo gli scheletri di buio",
+    
+    "Lore, mi hai rubato il cavallo?",
+    
+    "Il mandolino del barbone",
+    
+    "La neve rinforzata",
+    
+    "Sei un nano Ale?",
+    
+    "Qua ci sono rumori di radiazioni",
+    
+    "I mattoni finiscono come la merda",
+    
+    "Sono una chat di Twitch vivente",
+    
+    "È enormico",
+    
+    "Sembri lil Pipa",
+    
+    "Il cotechismo",
+    
+    "Noi ci chiediamo la domanda",
+    
+    "L'anagorismo analogico",
+    
+    "Ora divento collaterale",
+    "Il lubrificante è fatto di cazzo",
+    
+    "Il cambio in elevazione",
+    
+    "È egotistica",
+    
+    "Qual'è la differenza tra bucket e bajijumping",
+    
+    "Com'è che si chiamava il tachiometro?",
+    
+    "Tu sei Gucci, io sono Luis Fronz",
+    
+    "Perché stai sanguinando estensiamente?",
+    
+    "Sissi dai",
+    
+    "Mi sto interaldo",
+  
+    "Cavo tu devi assumere il ruolo di Elon Musk nel nuovo nostro pornazzo",
+  
+    "Hai 92 trifoni",
+  
+    "Ti piangerai i pantagioni",
+  
+     "La mia home di YouTube è piena di bambini",
+  
+     "Perché la mia faccia è tipo uno cerbiatto",
+  
+     "Giovanno YouTube",
+  
+  "Ha avuto la pressione",
+  
+   "Sottoliametnezioni",
+  
+  "Ha crashato lungo",
+  
+   "Queste variabili che contengono tutte e tre questo Gino dentro",
+  
+   "Simo sei tu quello che parli?",
+  
+  "Sali sesamo",
+  
+  "Il bombardamento sociale",
+  
+  "Sono specialmente drogato oggi",
+  
+   "I modem a vapore",
+  
+   "Dobbiamo usare i Pink Floyd come elemento?",
+  
+   "La L moscia",
+  
+  "DIO BROSTOLA",
+  
+  "La parte sopra dei capelli di gab sembra una crociera",
+  
+  "Gab ha un metro di ginocchia",
+  
+  "L'autismo immaculato",
+  
+  "Vuoi sia molto vocale",
+  
+  "Devo allacciarmi le gambe",
+  
+  "Io sono il grande dio cane di tutti i masterizzatori dei cani",
+  
+  "Grazie non sento niente, ho l'alzheimer",
+  
+  "Già fai i preventivi prima che parli",
+  
+  "È visioso",
+   
+  "So il collo di piedi",
+  
+  "Questa guerra dell'acqua è anche detta guerra dell'acqua",
+  
+  "Il calore è potente",
+  
+  "Che si sotterrino",
+  
+  "Immagina avere un capezzolo ogni limbo",
+  
+  "La portatile da RAM",
+  
+  "Cos'è questo suono di ignoranza",
+  
+  "Voi scherzate cagate",
+  
+  "I pesci acquatici",
+  
+  "Noo mi si è bloccato raga",
+  
+  "Il tubo catolico",
 
-  "Raga shrekko",
+  "Sto deportando il pc",
 
+  "Un cavo prolungamento",
 
+ "Le carde SD",
 
-  "L'elicottero sta discendendo!",
+ "Ti carete il piscio",
 
+ "Katana no pianta",
 
+ "Il tema di quando",
 
-  "C'è pioggia tanta.",
+ "È nelle mie corde vocali sbagliare",
 
+ "La mia situazione corrente è che sono in un     secchio",
 
+ "Come faccio a chiedere una domanda" ,
 
-  "Cavo ti rendi conto che è tutto pavimento?",
+"Perchè capriolo",
 
+"Hai acceso il lavandino",
 
+"Ditemi quando",
 
-  "Sei stato invitato al seguente invito.",
+"Le ventole salgono all'esplosione",
 
+"Oggi c'è tipo il sole di Dio",
 
+"Io ti dico una domanda e tu mi rispondi, nonono sto aggiornando Twitch",
 
-  "Ho i denti concavi.",
+"Hai finito il path of pain e non riesci a fare questo livello? Eh certo questi sono mobili",
 
+"I segni daltonici",
 
+"Un cavo prolungamento? Ok, no capito",
 
-  "Fartboot",
+"Come li cristo sti schivo?!",
 
+"Può usare il microfono come webcam",
 
+"Insieme formate il Power Ranger dei bastardi",
 
-  "Adesso Sesso",
+"È una crema per gli occhi",
 
+"Secondo me Mattia è un presepe",
 
+"Quante insolenze puoi dire alla gente",
 
-  "Sesso Adesso",
+"Non è razzismo è oggettività",
 
+"Non esiste il rumore dell'italiano in italiano",
 
+"Le tute antisommozzatura",
 
-  "Ma sissi dai!",
+"Le triple esplosioni, le quadruple madonne",
 
-  
+"È tempo di fare il ditalino con il cazzo",
 
-  "C'è odore di puzza qua dentro",
+"Sono i 4 cavalieri dell'apocalisse: la morte, la peste, la fame e la droga",
 
-  
+"Qualcuno mi ha mangiato l'acqua",
 
-  "Hanno chiuso le discoteche e le scuole no pero' sono la stessa cosa",
+"È nelle mie corde vocali sbagliare",
 
+"Google ti prescrive i gatti",
   
+"Che cazzo c'è ne frega che la Mariarosa va a prendere lo stendibiancheria",
 
-  "È Despair",
-  
-  
-  "Gli scambi commerciali",
-  
-  
-  "Conte ha detto che il 100% delle lezioni online saranno a scuola",
-  
-  
-  "Dai Bob, droppa le piastrelle",
-  
-  
-  "Parla e stai zitto",
-  
-  
-  "È chiaro che sono gay se lo spammi 400 volte",
-  
-  
-  "Volete vedere il mio tavolo potabile?",
-  
-  
-  "Hey guys, temperatures are pretty low here VAFFANCULO UAAAAAAAAAAAAAAAAAAAAA",
-  
-  "Simo tu domani lo porti il grembiule?",
-  
-  "Il mitico salto del papa",
-  
-  "Fammi vedere il retto di fuoco",
-  
-  "Raga il mirino elettrico",
-  
-  "Forse stava indossando un costume da gay",
-  
-  "Voglio piangere mentre esplodo",
-  
-  "Perché, come sapete, mia madre è la basilicata",
-  
-  "Ho una voglia esplodente",
-  
-  "Quando esce da questo if del sesso",
-  
-  "Simo ha tipo tre porte di finestre",
-  
-  "Ma le porte finestre sono quelle che portano le finestre",
-  
-  "Jacopo, casa tua è scale",
-  
-  "Gab, quanti alberi hai crocifisso?",
-  
-  "Sto uccidendo gli scheletri di buio",
-  
-  "Lore, mi hai rubato il cavallo?",
-  
-  "Il mandolino del barbone",
-  
-  "La neve rinforzata",
-  
-  "Sei un nano Ale?",
-  
-  "Qua ci sono rumori di radiazioni",
-  
-  "I mattoni finiscono come la merda",
-  
-  "Sono una chat di Twitch vivente",
-  
-  "È enormico",
-  
-  "Sembri lil Pipa",
-  
-  "Il cotechismo",
-  
-  "Noi ci chiediamo la domanda",
-  
-  "L'anagorismo analogico",
-  
-  "Ora divento collaterale",
-  "Il lubrificante è fatto di cazzo",
-  
-  "Il cambio in elevazione",
-  
-  "È egotistica",
-  
-  "Qual'è la differenza tra bucket e bajijumping",
-  
-  "Com'è che si chiamava il tachiometro?",
-  
-  "Tu sei Gucci, io sono Luis Fronz",
-  
-  "Perché stai sangunando estensiamente?",
-  
-  "Sissi dai",
-  
-  "Mi sto interaldo",
-  
+"Le ho mostrato il pene gentilmente",
 
+"Gab è un fuoristrada",
 
+"Raga, stiamo bruciando di freddo",
+
+"Come commettere suicidio in 2 passi",
+
+"Sto bevendo un anurismo",
+
+"Che regole vuoi che sia",
+
+"In lavoro di gruppo fai fatica a fare le cose da solo",
+
+"Salve, questa è una rapina",
+
+"Quando mana la cosa",
+
+"Perché ogni umano prova piacere nel provocarsi dolore",
+
+"Vabbè quella è una femmina non vale niente",
+
+"Le seghe ma godute molto belle",
+
+"Ha detto tipo casi di mafia fratto la droga",
+
+"Sai che sei requa?",
+
+"Sei un procione con lo sperghers",
+
+"Apollo figlio di Apostolo",
+
+"Quel gioco è fatto apposta per farti vivere la vita di Mussolini",
+
+"La roulotte russa",
+
+"Si vede che tra poco tocchi la Luna senza usare le mani",
+
+"Prendi una palasse al cervello",
+
+"Vecchie fiamme che ritornano a lavorare",
+
+"È un processore fa i calcoli",
+
+"Per entrare dalla finestra devi attraversare robe e pali",
+
+"Among Us",
+
+"Siete come dei bambini diabetici con un pacco di MnM",
+
+"Sono falsi come il giovedì il sabato",
+
+"Scusa non ti ho sentito, stavo mangiando una patatina",
+
+"Lo hanno anche boostato, ora ha anche due palle di cancro nero",
+
+"Mi si stan glitchando anche le orecchie",
+
+"Ha del lavoro sulle spalle",
+
+"Pier cosa stai guardato",
+
+"Scrivo a tempo di Amogus",
+
+"Apex ha fatto: solo trio, tranne tutte le modalità apparte duo",
 
 ];
 
-
-
-var cit = [
-
-
-
-  "Cavo, 2020",
+  var cit = [
 
 
 
-  "MatPrayer, 2020",
-
-
-
-  "MatPrayer, 2020",
-
-
-
-  "Simone, 2020",
-
-
-
-  "Lore, 2020",
-
-
-
-  "Gab, 2020",
-
-
-
-  "Tommasyno, 2020",
-
-
-
-  "Simone, 2020",
-
-
-
-  "Simone, 2020",
-
-
-
-  "Simone, 2020",
-
+    "Cavo, 2020",
   
-
-  "Gab, 2020",
-
   
-
-  "Cavo, 2020",
-
   
-
-  "Tutti, 2020",
+    "MatPrayer, 2020",
   
-
+  
+  
+    "MatPrayer, 2020",
+  
+  
+  
+    "Simone, 2020",
+  
+  
+  
+    "Lore, 2020",
+  
+  
+  
+    "Gab, 2020",
+  
+  
+  
+    "Tommasyno, 2020",
+  
+  
+  
+    "Simone, 2020",
+  
+  
+  
+    "Simone, 2020",
+  
+  
+  
+    "Simone, 2020",
+  
+    
+  
+    "Gab, 2020",
+  
+    
+  
+    "Cavo, 2020",
+  
+    
+  
+    "Tutti, 2020",
+    
+  
+      "Gab, 2020",
+      
+      
+      "Gab, 2020",
+      
+      
+      "Gab, 2020",
+      
+      
+      "Simone, 2020",
+      
+      
+      "Cavo, 2020",
+      
+      
+      "Gab, 2020",
+      
+      
+      "Cavo, 2020",
+      
+      "Cavo, 2020",
+      
+      "Pier, 2020",
+      
+      "Pier, 2020",
+      
+      "Simo, 2020",
+      
+      "Gab, 2020",
+      
+      "Gab, 2020",
+      
+      "Cavo, 2020",
+    
+      "Gab, 2020",
+    
+      "Vallesi, 2020",
+    
+      "Gab, 2020",
+    
+      "Gab, 2020",
+      
+      "Gab, 2020",
+    
+      "Cavo, 2020",
+    
+      "Gab, 2020",
+    
+      "Andre, 2020",
+    
     "Gab, 2020",
     
-    
     "Gab, 2020",
     
+    "Lore, 2020",
     
     "Gab, 2020",
-    
     
     "Simone, 2020",
     
-    
-    "Cavo, 2020",
-    
-    
     "Gab, 2020",
     
-    
-    "Cavo, 2020",
-    
-    "Cavo, 2020",
-    
-    "Pier, 2020",
-    
-    "Pier, 2020",
-    
-    "Simo, 2020",
+    "Bob, 2017",
     
     "Gab, 2020",
     
     "Gab, 2020",
     
-    "Cavo, 2020",
-  
-    "Gab, 2020",
-  
-    "Vallesi, 2020",
-  
-    "Gab, 2020",
-  
     "Gab, 2020",
     
     "Gab, 2020",
-  
-    "Cavo, 2020",
-  
+    
     "Gab, 2020",
+    
+    "Cavo, 2021",
+    
+    "Gab, 2020",
+    
+    "Gab, 2020",
+    
+    "Amy, 2020",
+    
+    "Gab, 2021",
+    
+    "Gab, 2020",
+    
+    "Gab, 2021",
+    
+    "Vallesi, 2021",
+    
+    "Lore, 2020",
   
-    "Andre, 2020",
+    "Gab, 2021",
   
-  "Gab, 2020",
+    "Gab, 2021",
   
-  "Gab, 2020",
+    "Gab, 2021",
   
-  "Lore, 2020",
+    "Cavo, 2019",
   
-  "Gab, 2020",
+    "Gab, 2021",
   
-  "Simone, 2020",
-  
-  "Gab, 2020",
-  
-  "Bob, 2017",
-  
-  "Gab, 2020",
-  
-  "Gab, 2020",
-  
-  "Gab, 2020",
-  
-  "Gab, 2020",
-  
-  "Gab, 2020",
-  
-  "Cavo 2021",
-  
-  "Gab, 2020",
-  
-  "Gab, 2020",
-  
-  "Amy, 2020",
+    "Gab, 2021",
   
   "Gab, 2021",
   
-  "Gab, 2020",
+   "Leti, 2020",
+  
+  "Bassi, 2021",
+  
+   "Vallesi. 2021",
+  
+   "Gab, 2021",
   
   "Gab, 2021",
   
-  "Vallesi, 2021",
+  "Gab, 2021",
   
-  "Gab, 2020",
+  "Gab, 2021",
   
-]
+   "Simone, 2021",
+  
+   "Cavo, 2021",
+  
+   "Gab, 2021",
+  
+  "Gab, 2021",
+  
+   "Cavo, 2021",
+  
+  "MatPrayer, 2021",
+  
+  "Gab, 2021",
+  
+  "Gab, 2021",
+  
+  "Gab, 2021", 
+  
+  "Gab, 2021", 
+  
+  "Gab, 2021", 
+  
+  "Gab, 2021", 
+  
+  "Gab, 2021", 
+   
+  "Simo, 2021",
+  
+  "Cavo, 2021",
+  
+   "Cavo, 2021",
+  
+  "Bassi, 2021",
+  
+  "Gab, 2021", 
+   
+  "Cavo, 2021",
+  
+  "Bob, 2021",
+  
+  "Gab, 2021",
+  
+  "Gab, 2021",  
+  
+  "Diego, 2021",
+  
+  "Gab, 2021",
 
+  "Gab, 2021",
+
+ "Gab, 2021",
+
+ "Cavo, 2021",
+
+ "Gab, 2021",
+
+"Simo, 2021",
+
+"Andre, 2021",
+
+"Cavo, 2021",
+
+ "Gab, 2020",
+
+ "Gab, 2021",
+
+"Cavo, 2021",
+
+"Gab, 2021",
+
+"Simo, 2021",
+
+"MatPrayer, 2021",
+
+"Cavo, 2021",
+
+"Gab, 2021",
+
+"Bob, 2021",
+
+"Andre, 2021",
+
+"Gab, 2021",
+
+"Bob, 2021",
+
+"Gab, 2021",
+
+"Gab, 2021",
+
+"Abdul, 2017",
+
+"Simone, 2021",
+
+"Simone, 2021",
+
+"Celli, 2021",
+
+"Gab, 2021",
+
+"Celli, 2021",
+
+"Pier, 2021",
+
+"Cavo, 2021",
+
+"Pier, 2021",
+
+"Cavo, 2021",
+
+"Cavo, 2021",
+
+"Leti, 2021",
+
+"Cavo, 2021",
+
+"Simo, 2021",
+
+"Bob, 2021",
+
+"Pier, 2021",
+
+"Gab, 2021",
+
+"Gab, 2021",
+
+"Gab, 2021",
+
+"Simo, 2021",
+
+"Cavo, 2021",
+
+"Bob, 2021",
+
+"Bob, 2021",
+
+"Bob, 2017",
+
+"Gab, 2021",
+
+"Pier, 2021",
+
+"Gab, 2021",
+
+"Gab, 2021",
+
+"Gab, 2021",
+
+"Cavo, 2021",
+
+"Andre, 2021",
+
+"Pier, 2021",
+
+"Gab, 2021",
+
+"Pier, 2021",
+
+"Cavo, 2021",
+
+"Cavo, 2021",
+
+"Bob, 2021",
+
+"Gab, 2021",
+
+"Pier, 2021",
+
+"Despair, 2021",
+
+"Pier, 2021",
+
+"Pier, 2021",
+
+"Pier, 2021",
+
+"Gab, 2021",
+
+"Pier, 2021",
+
+"Cavo, 2021"
+];
 
 
 //Non va fatto così, ma funziona
